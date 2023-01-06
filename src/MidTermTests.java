938a194357dbc2bdd88abdbff6acf33d2cfc0361
thishastobenoah <thishastobenoah@gmail.com>
2023-01-05 21:17:32 -0500

    added 1 test

--- src/MidTermTests.java
+++ src/MidTermTests.java
@@ -1,4 +1,11 @@
 
 public class MidTermTests {
-
+	@Test
+	void test1() {
+		Product product1 = new Product ("Batman Comic","Books", "A Comic book about Batman!", 19.99);
+		AssertEquals("Batman Comic", product1.getName());
+		AssertEquals("Books", product1.getCategory());
+		AssertEquals("A Comic book about Batman!", product1.getDescription());
+		AssertEquals(19.99, product1.getPrice());
+	}
 }
