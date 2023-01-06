50d9a832a3426741e2b2d8eb510b1450038eab9b
ab <abdullahbilbaria@yahoo.com>
2023-01-05 20:31:44 -0500

    add files

---
+++ src/Payment.java
@@ -0,0 +1,8 @@
+
+public abstract class Payment {
+	
+	  protected double taxAmount;
+	  protected double subtotal;
+	  protected double total;
+	  //Sure will need to add a Order class to call here 
+}
