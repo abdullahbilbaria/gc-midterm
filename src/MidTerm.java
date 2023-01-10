22b0c1aeba1ff19976956ecb09be00145b70c4c8
thishastobenoah <thishastobenoah@gmail.com>
2023-01-09 20:44:20 -0500

    main method update

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class MidTerm {
 
@@ -29,7 +30,75 @@ public class MidTerm {
 		products.add(product11);
 		Product product12 = new Product ("Guardians of the Galaxy Poster", "Posters","A Poster of the Guardians of the Galaxy", 5.00);
 		products.add(product12);
+		//////////////////////////////////////////////////////////////////////////////////////
+		List<Product> shoppingCart = new ArrayList<>();
+		String loopResponse;
+		int checkNumber;
+		double cash;
+		int CC;
+		String expiration;
+		int CVV;
+//		Double subtotal = calcSubtotal(shoppingCart);
+//		Double salesTax = calcSalesTax(shoppingCart);
+//		Double grandTotal = calcGrandTotal(shoppingCart);
+		Scanner scnr = new Scanner (System.in);
+		System.out.println("Welcome to Grand Circus Comic Book Shop!!");
+		do{
+			for (int i = 0; i < products.size(); i++) {
+		
+			System.out.println((i+1) + ""  + products.get(i));
+			}
+			System.out.println("What would you like to purchase?");
+			int userInput = scnr.nextInt();
+			System.out.println("How many would you like?");
+			int itemCount = scnr.nextInt();
+			System.out.println(products.get(userInput) + ":" + products.get(userInput).getPrice()* itemCount);
+			for (int i = 0; i < itemCount; i++) {
+				shoppingCart.add(products.get(userInput));
+			}
+			System.out.print("Continue shopping?(Y/N):");
+			loopResponse  = scnr.nextLine();
+		}while (loopResponse.equalsIgnoreCase("y"));
+//		System.out.println("Subtotal: " + subtotal); 
+//		System.out.println("Sales Tax: " + salesTax);
+//		System.out.println("Grand Total: " + grandTotal);
+		
+		System.out.println("How  would you like to pay?: 1. Cash, 2. Check, 3. Credit");
+		int paymentMethod = scnr.nextInt();
+//		switch(paymentMethod) {
+//		case 1:
+//			break;
+//		case 2:
+//			
+//			break;
+//		case 3:
+//			
+//		}
+// need to implement abstract pay methods
+
+		System.out.println("______RECEIPT_____");
+		System.out.println("==================");
+		System.out.println(shoppingCart);
+		System.out.println("==================");
+//		System.out.println("Subtotal:" + subtotal);
+//		System.out.println("Sales Tax:" + salesTax);
+//		System.out.println("Grand Total:" + grandTotal);
+		switch(paymentMethod) {
+		case 0:
+			System.out.println("Paid: " + cash);
+//			System.out.println("Change: " + (cash - grandTotal));
+			break;
+		case 1:
+			System.out.println("Check:" + checkNumber);
+			break;
+		case 2:
+			System.out.println("CC:" + CC);
+			System.out.println("EXP:" + expiration);
+			System.out.println("CVV:" + CVV);
+			break;
+
+		}
+	
 
 	}
-
 }
